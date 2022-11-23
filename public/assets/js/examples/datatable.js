ae052a4dd94c0142f74d192506a186326a859538
Dr.GHOST <phichitonline@gmail.com>
2022-11-23 14:25:02 +0700

    DataTable DataExport

--- public/assets/js/examples/datatable.js
+++ public/assets/js/examples/datatable.js
@@ -18,6 +18,32 @@ $(document).ready(function () {
 			}
         }
     });
+    $('#example2').DataTable({
+        "oLanguage": {
+			"sProcessing": "กำลังประมวลผล...",
+			"sLengthMenu": "แสดง _MENU_ รายการต่อหน้า",
+			"sZeroRecords": "ไม่พบข้อมูล",
+			"sInfo": "แสดง _START_ ถึง _END_ จาก _TOTAL_ รายการ",
+			"sInfoEmpty": "แสดง 0 ถึง 0 ของ 0 รายการ",
+			"sInfoFiltered": "(จากทั้งหมด _MAX_ รายการ)",
+			"sSearch": "ค้นหา :",
+			"oPaginate": {
+				"sFirst": "เริ่มต้น",
+				"sPrevious": "ก่อนหน้า",
+				"sNext": "ถัดไป",
+				"sLast": "สุดท้าย"
+			}
+        }
+    });
+    $('#example3').DataTable({
+        // responsive: true
+    });
+
+    $('#example4').DataTable({
+        "scrollY": "200px",
+        "scrollCollapse": true
+    });
+
     $('#DataExport').DataTable({
         responsive: true,
         "pageLength": 10,
@@ -38,14 +64,6 @@ $(document).ready(function () {
 				"sLast": "สุดท้าย"
 			}
 		}
-    });
-    $('#example3').DataTable({
-        // responsive: true
-    });
-
-    $('#example4').DataTable({
-        "scrollY": "200px",
-        "scrollCollapse": true
     });
 
 });
